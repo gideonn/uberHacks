ea4648cc56366c2cacdfbe6f0524fe4ae57e2fbd
Abhishek Srivastava <srivastava.abhishek.sa@gmail.com>
2018-02-19 17:04:01 -0600

    Added loop

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import json
 import requests
 import re
+import time
+import threading
 from twilio.rest import Client
 
 
@@ -12,7 +14,8 @@ class Uber:
         self.start_loc = []
         self.end_loc = []
         self.configDict = self.loadConfig(config_file)
-        self.threshold = 0
+        self.currPrice = 0.0
+        self.threshold = 0.0
 
 
     def loadConfig(self, config_file):
@@ -37,10 +40,10 @@ class Uber:
 
 
     def getUserLocationsAndThreshold(self):
-        add1 = self.urlify(input("Enter the \"FROM\" address:"))
-        add2 = self.urlify(input("Enter the \"TO\" address:"))
+        add1 = self.urlify(input("Enter the \"FROM\" address: "))
+        add2 = self.urlify(input("Enter the \"TO\" address: "))
 
-        self.threshold = input("Enter the threshold amount:")
+        self.threshold = float(input("Enter the threshold amount:"))
         from_lat, from_lon = self.getLatLonFromAdd(add1)
         to_lat, to_lon = self.getLatLonFromAdd(add2)
 
@@ -63,45 +66,57 @@ class Uber:
 
 
     #loc1 and loc2 are list. Indexes 0 and 1 are latitude and longitude
-    def getReqObject(self, prodID):
+    def getPrice(self, prodID):
         headers = {'Authorization': "Bearer " + self.configDict['uber_access_token'],'Accept - Language':'en_US',
                    'Content-Type':'application/json'}
         payload = {'product_id':'712b9d70-1254-4268-a275-33e487a4a54c','seats':'1','start_latitude': self.start_loc[0], 'start_longitude': self.start_loc[1],'end_latitude':self.end_loc[0],'end_longitude':self.end_loc[1]}
 
-        r = requests.post("https://api.uber.com/v1.2/requests/estimate", data=json.dumps(payload), headers=headers)
-
-        return r
-
-
-    def getPrice(self,r):
-        data = r.json()
-        print("Current price of Uber POOL is ", data['fare']['value'])
-        return data['fare']['value']
-
-
-    def isBelowThreshold(self,currPrice):
-        if currPrice <= self.threshold:
-            print("Current price : $", currPrice, " is less than the set threshold: $", self.threshold)
-            decision = input("Should I go ahead and book the cab?")
-            if decision == 'Y':
-                resp = self.bookCab()
-                #if resp == success, send an SMS notification
-
-                #sleep for 15 seconds, fetch the cab booked details and send an SMS with that info
-                details = self.getCabDetails()
+        try:
+            r = requests.post("https://api.uber.com/v1.2/requests/estimate", data=json.dumps(payload), headers=headers)
+            if r.status_code == 200:
+                data = r.json()
+                print("Current price of Uber POOL is ", data['fare']['value'])
+                self.currPrice = float(data['fare']['value'])
+                return r
             else:
-                print("Okay, not booking the cab. Retrying in a while...")
-                #Ask to update threshold value
-        else:
-            print("Current price :", currPrice, " is more than the set threshold: ", self.threshold)
-            print("Retrying in a while...")
+                print("Error in calling Uber cab lookup API")
+                exit(-1)
+        except:
+            print("Couldn't call API, exiting...")
+            exit(-1)
 
+
+    def checkAndBookCab(self):
+        try:
+            while True:
+                if self.currPrice <= self.threshold:
+                    print("Current price : $", self.currPrice, " is less than the set threshold: $", self.threshold)
+                    decision = input("Should I go ahead and book the cab?")
+                    if decision == 'Y':
+                        resp = self.confirmCab()
+                        #if resp == success, send an SMS notification
+
+                        #sleep for 15 seconds, fetch the cab booked details and send an SMS with that info
+                        details = self.getCabDetails()
+                    else:
+                        print("Okay, not booking the cab. Retrying in a while...")
+                        #TODO
+                        # print("Do you want to update the threshold value ?")
+                        self.getPrice('')
+                        #Ask to update threshold value
+                else:
+                    print("Current price :", self.currPrice, " is more than the set threshold: ", self.threshold)
+                    print("Retrying in a while...")
+                print("Press Ctrl+C to exit anytime you want the script to stop...")
+                time.sleep(self.configDict['freq_check'])
+        except:
+            exit(0)
 
     def notifyUser(cell, message):
         pass
 
 
-    def bookCab(self):
+    def confirmCab(self):
         pass
 
 
@@ -115,10 +130,8 @@ if __name__ == '__main__':
     # configDict = loadConfig(config_file)
 
     uber_inst = Uber(config_file)
-
     uber_inst.getUserLocationsAndThreshold()
-    reqObj = uber_inst.getReqObject('todo-prodID')
+    uber_inst.getPrice('todo-prodID')
 
-    currPrice = uber_inst.getPrice(reqObj)
-
+    uber_inst.checkAndBookCab()
 
